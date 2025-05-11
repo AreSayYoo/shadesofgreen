6f7a7de3231e63bb0ea2d02fc35fe53d82675037
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-05-10 22:39:13 -0400

    building sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://shadesofgreen.nz", // 🔁 Replace with your actual domain
+  generateRobotsTxt: true, // ✅ Will also create robots.txt
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};
