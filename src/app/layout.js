b3f00ca81d5ed2619160d24239747bccfd297e93
Matt Arceo <32740884+AreSayYoo@users.noreply.github.com>
2025-09-29 07:34:29 -0400

    adjusted file structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,7 @@
-"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import React, { useState, useRef } from "react";
-import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import img from "./images/img.jpg";
-import img1 from "./images/img1.jpg";
-import img2 from "./images/img2.jpg";
-import img3 from "./images/img3.jpg";
-import img4 from "./images/img4.jpg";
-import Logo from "./images/Logonobg.png";
-import messenger from "./images/Facebook_Messenger_logo.png";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,54 +14,16 @@ const geistMono = Geist_Mono({
 });
 
 export default function RootLayout({ children }) {
-  const [formStatus, setFormStatus] = useState({ message: "", success: null });
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setFormStatus({ message: "Sending message...", success: null });
-    setIsPopupOpen(true); // Show popup while sending
-
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        access_key: "fea598f9-3f42-4f51-b60a-8be4283fc878",
-        name: e.target.name.value,
-        email: e.target.email.value,
-        message: e.target.message.value,
-      }),
-    });
-    const result = await response.json();
-    if (result.success) {
-      setFormStatus({ message: "Message sent successfully!", success: true });
-      e.target.reset();
-    } else {
-      setFormStatus({
-        message:
-          "Something went wrong...Please try again. Or message us on Facebook",
-        success: false,
-      });
-    }
-
-    setIsPopupOpen(true); // Ensure popup shows message
-  }
-
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {/* ✅ Google Site Verification */}
         <meta
           name="google-site-verification"
           content="xgyp8Pui_eg6big_xu-21aE2NxE-BDQbBEMWzAh7LDg"
         />
-
         <title>Shades of Green – Lawns & Gardens | Kāpiti Coast</title>
         <meta
           name="description"
@@ -83,8 +34,6 @@ export default function RootLayout({ children }) {
           content="Kapiti lawn care, Shades of Green, garden maintenance, lawn mowing, green waste, Kapiti landscaping, New Zealand gardening, lawn services, garden services, lawn care"
         />
         <meta name="author" content="Shades of Green – Lawns & Gardens" />
-
-        {/* Open Graph / Facebook */}
         <meta property="og:title" content="Shades of Green – Lawns & Gardens" />
         <meta
           property="og:description"
@@ -93,255 +42,13 @@ export default function RootLayout({ children }) {
         <meta property="og:image" content="/images/img3.jpg" />
         <meta property="og:url" content="https://shadesofgreen.nz" />
         <meta property="og:type" content="website" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="app-container">
-          <div className="floating-buttons">
-            <a href="#top" className="floating-button">
-              <span class="material-icons">home</span>
-            </a>
-            <a href="#services" className="floating-button">
-              <span className="material-icons">build</span>
-            </a>
-            <a href="#about" className="floating-button">
-              <span className="material-icons">info</span>
-            </a>
-            <a href="#contact" className="floating-button">
-              <span className="material-icons">mail</span>
-            </a>
-            <a
-              href="https://www.facebook.com/shadesofgreenkapiti/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="floating-button"
-            >
-              <span className="material-icons">facebook</span>
-            </a>
-          </div>
-
-          <main>
-            <div className="logo-container">
-              <Image
-                src={Logo}
-                alt="Shades of Green Logo"
-                className="logo-image"
-              />
-            </div>
-            <section id="services" className="services-section">
-              <h1 className="services-title">Our Services</h1>
-              <div className="services-list">
-                {[
-                  { icon: "🌱", text: "Lawn Mowing" },
-                  { icon: "🌿", text: "Fertilization" },
-                  { icon: "🚜", text: "Weed Control" },
-                  { icon: "🌼", text: "Garden Maintenance" },
-                  { icon: "♻️", text: "Green Waste Removal" },
-                  { icon: "🍂", text: "Seasonal Cleanup" },
-                ].map((service, index) => (
-                  <div key={index} className="service-item">
-                    <span className="service-icon">{service.icon}</span>
-                    <span className="service-text">{service.text}</span>
-                  </div>
-                ))}
-              </div>
-            </section>
-            <Carousel id="pictures" className="carousel">
-              <Carousel.Item>
-                <Image
-                  className="d-block w-100 carousel-img"
-                  src={img3}
-                  alt="Lawn care 1"
-                />
-                <Carousel.Caption>
-                  <h3></h3>
-                  <p></p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image
-                  className="d-block w-100 carousel-img"
-                  src={img1}
-                  alt="Lawn care 2"
-                />
-                <Carousel.Caption>
-                  <h3></h3>
-                  <p></p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image
-                  className="d-block w-100 carousel-img"
-                  src={img2}
-                  alt="Lawn care 3"
-                />
-                <Carousel.Caption>
-                  <h3></h3>
-                  <p></p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image
-                  className="d-block w-100 carousel-img"
-                  src={img}
-                  alt="Lawn care 4"
-                />
-                <Carousel.Caption>
-                  <h3></h3>
-                  <p></p>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item>
-                <Image
-                  className="d-block w-100 carousel-img"
-                  src={img4}
-                  alt="Lawn care 5"
-                />
-                <Carousel.Caption>
-                  <h3></h3>
-                  <p></p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-            <br></br>
-
-            <section id="about" className="about">
-              <h1>About Shades of Green</h1>
-              <div className="about-section">
-                <p>
-                  After calling the beautiful Kāpiti Coast home for over 20
-                  years, I decided to turn my passion for well-kept lawns and
-                  gardens into a business—so I started{" "}
-                  <strong>Shades of Green – Lawns & Gardens</strong>.
-                </p>
-
-                <p>
-                  Fast forward three years, and I’m still out there, rain or
-                  shine, keeping Kāpiti’s backyards looking their best. I’m
-                  truly grateful for all my fantastic clients who’ve backed me
-                  along the way—it’s been a pleasure helping you keep your
-                  greenspaces tidy and thriving.
-                </p>
-
-                <p>
-                  If your lawn’s getting a bit wild or the garden needs a bit of
-                  TLC, give me a call at{" "}
-                  <a href="tel:0221725508">022 172 5508</a> or flick me an email
-                  at{" "}
-                  <a href="mailto:lane@shadesofgreen.nz">
-                    lane@shadesofgreen.nz
-                  </a>
-                  . Whether it’s a regular mow, a bit of garden maintenance, or
-                  a one-off tidy-up, I’ve got you covered.
-                </p>
-
-                <p>
-                  So, why spend your weekends battling the backyard when you
-                  could be out enjoying it? Let me take care of the hard yakka
-                  while you sit back and relax.
-                </p>
-
-                <p>
-                  <em>More to come—cheers!</em>
-                </p>
-
-                <p>
-                  <strong>Lane</strong>
-                </p>
-              </div>
-            </section>
-
-            <section id="contact" className="contact-section">
-              <h1>Contact Us</h1>
-              <form className="contact-form" onSubmit={handleSubmit}>
-                <input
-                  type="hidden"
-                  name="access_key"
-                  value="fea598f9-3f42-4f51-b60a-8be4283fc878"
-                />
-                <input
-                  type="hidden"
-                  name="subject"
-                  value="New Submission from Shades of Green Website"
-                />
-                <input
-                  type="hidden"
-                  name="from_name"
-                  value="Shades of Green Kapiti"
-                />
-                <input
-                  type="hidden"
-                  name="ccemail"
-                  value="matt.p.arceo@gmail.com"
-                />
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required />
-
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-
-                <label htmlFor="message">Message:</label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  required
-                ></textarea>
-
-                <button type="submit">Send Message</button>
-              </form>
-              {/* Success/Error Message Display */}
-              {isPopupOpen && (
-                <div
-                  className="popup-overlay"
-                  onClick={() => setIsPopupOpen(false)}
-                >
-                  <div
-                    className="popup-content"
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    <button
-                      className="close-popup"
-                      onClick={() => setIsPopupOpen(false)}
-                    >
-                      ✖
-                    </button>
-                    <p
-                      className={
-                        formStatus.success ? "success-message" : "error-message"
-                      }
-                    >
-                      {formStatus.message}
-                    </p>
-                  </div>
-                </div>
-              )}
-
-              <div class="messenger-link">
-                <a
-                  href="https://m.me/shadesofgreenkapiti"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <Image
-                    src={messenger}
-                    alt="Message us on Messenger"
-                    className="messenger-icon"
-                  />
-                  Message Us On Facebook
-                </a>
-              </div>
-            </section>
-          </main>
-
-          <footer className="footer">
-            <p>
-              &copy; {new Date().getFullYear()} Shades of Green. All rights
-              reserved.
-            </p>
-          </footer>
-        </div>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,100 +1,248 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { Carousel } from "react-bootstrap";
+
+import Logo from "./images/Logonobg.png";
+import img from "./images/img.jpg";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import messenger from "./images/Facebook_Messenger_logo.png";
+
+const services = [
+  { icon: "🌱", text: "Lawn Mowing" },
+  { icon: "🌿", text: "Fertilization" },
+  { icon: "🚜", text: "Weed Control" },
+  { icon: "🌼", text: "Garden Maintenance" },
+  { icon: "♻️", text: "Green Waste Removal" },
+  { icon: "🍂", text: "Seasonal Cleanup" },
+];
+
+const carouselImages = [img3, img1, img2, img, img4];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [formStatus, setFormStatus] = useState({ message: "", success: null });
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const { name, email, message } = form;
+
+    setFormStatus({ message: "Sending message...", success: null });
+    setIsPopupOpen(true);
+
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          access_key: "fea598f9-3f42-4f51-b60a-8be4283fc878",
+          name: name.value,
+          email: email.value,
+          message: message.value,
+        }),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        setFormStatus({
+          message: "Message sent successfully!",
+          success: true,
+        });
+        form.reset();
+      } else {
+        setFormStatus({
+          message:
+            "Something went wrong...Please try again. Or message us on Facebook",
+          success: false,
+        });
+      }
+    } catch (error) {
+      setFormStatus({
+        message:
+          "We couldn't submit the form. Please try again later or message us on Facebook.",
+        success: false,
+      });
+    }
+
+    setIsPopupOpen(true);
+  }
+
+  return (
+    <div id="top" className="app-container">
+      <div className="floating-buttons">
+        <a href="#top" className="floating-button">
+          <span className="material-icons">home</span>
+        </a>
+        <a href="#services" className="floating-button">
+          <span className="material-icons">build</span>
+        </a>
+        <a href="#about" className="floating-button">
+          <span className="material-icons">info</span>
+        </a>
+        <a href="#contact" className="floating-button">
+          <span className="material-icons">mail</span>
+        </a>
+        <a
+          href="https://www.facebook.com/shadesofgreenkapiti/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="floating-button"
+        >
+          <span className="material-icons">facebook</span>
+        </a>
+      </div>
+
+      <main>
+        <div className="logo-container">
+          <Image src={Logo} alt="Shades of Green Logo" className="logo-image" />
         </div>
+
+        <section id="services" className="services-section">
+          <h1 className="services-title">Our Services</h1>
+          <div className="services-list">
+            {services.map((service) => (
+              <div key={service.text} className="service-item">
+                <span className="service-icon">{service.icon}</span>
+                <span className="service-text">{service.text}</span>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <Carousel id="pictures" className="carousel">
+          {carouselImages.map((imageSrc, index) => (
+            <Carousel.Item key={index}>
+              <Image
+                className="d-block w-100 carousel-img"
+                src={imageSrc}
+                alt={`Lawn care ${index + 1}`}
+              />
+              <Carousel.Caption>
+                <h3></h3>
+                <p></p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+
+        <section id="about" className="about">
+          <h1>About Shades of Green</h1>
+          <div className="about-section">
+            <p>
+              After calling the beautiful Kāpiti Coast home for over 20 years, I
+              decided to turn my passion for well-kept lawns and gardens into a
+              business, so I started <strong>Shades of Green – Lawns & Gardens</strong>.
+            </p>
+            <p>
+              Fast forward three years, and I’m still out there, rain or shine,
+              keeping Kāpiti’s backyards looking their best. I’m truly grateful
+              for all my fantastic clients who’ve backed me along the way—it’s
+              been a pleasure helping you keep your greenspaces tidy and thriving.
+            </p>
+            <p>
+              If your lawn’s getting a bit wild or the garden needs a bit of TLC,
+              give me a call at <a href="tel:0221725508">022 172 5508</a> or flick
+              me an email at
+              {" "}
+              <a href="mailto:lane@shadesofgreen.nz">lane@shadesofgreen.nz</a>. Whether
+              it’s a regular mow, a bit of garden maintenance, or a one-off tidy-up,
+              I’ve got you covered.
+            </p>
+            <p>
+              So, why spend your weekends battling the backyard when you could be
+              out enjoying it? Let me take care of the hard yakka while you sit
+              back and relax.
+            </p>
+            <p>
+              <em>More to come—cheers!</em>
+            </p>
+            <p>
+              <strong>Lane</strong>
+            </p>
+          </div>
+        </section>
+
+        <section id="contact" className="contact-section">
+          <h1>Contact Us</h1>
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <input
+              type="hidden"
+              name="access_key"
+              value="fea598f9-3f42-4f51-b60a-8be4283fc878"
+            />
+            <input
+              type="hidden"
+              name="subject"
+              value="New Submission from Shades of Green Website"
+            />
+            <input
+              type="hidden"
+              name="from_name"
+              value="Shades of Green Kapiti"
+            />
+            <input
+              type="hidden"
+              name="ccemail"
+              value="matt.p.arceo@gmail.com"
+            />
+
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <button type="submit">Send Message</button>
+          </form>
+
+          {isPopupOpen && (
+            <div className="popup-overlay" onClick={() => setIsPopupOpen(false)}>
+              <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+                <button className="close-popup" onClick={() => setIsPopupOpen(false)}>
+                  ✖
+                </button>
+                <p
+                  className={
+                    formStatus.success ? "success-message" : "error-message"
+                  }
+                >
+                  {formStatus.message}
+                </p>
+              </div>
+            </div>
+          )}
+
+          <div className="messenger-link">
+            <a
+              href="https://m.me/shadesofgreenkapiti"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src={messenger}
+                alt="Message us on Messenger"
+                className="messenger-icon"
+              />
+              Message Us On Facebook
+            </a>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="footer">
+        <p>&copy; {new Date().getFullYear()} Shades of Green. All rights reserved.</p>
       </footer>
     </div>
   );
